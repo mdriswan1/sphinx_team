239bc7653ffec727c74b6ee2a5a464bb6a7379f3
kamalraj51 <rkamalraj583@gmail.com>
2026-04-02 12:21:08 +0530

    kamal upload question

--- src/main/java/com/vastpro/restapi/RestApplication.java
+++ src/main/java/com/vastpro/restapi/RestApplication.java
@@ -2,6 +2,8 @@ package com.vastpro.restapi;
 
 import org.glassfish.jersey.server.ResourceConfig;
 
+import org.glassfish.jersey.media.multipart.MultiPartFeature;
+
 import org.glassfish.jersey.jackson.JacksonFeature;
 
 /**
@@ -16,5 +18,6 @@ public class RestApplication extends ResourceConfig{
     public RestApplication() {
         packages("com.vastpro.restapi.resources");
         register(JacksonFeature.class);
+        register(MultiPartFeature.class);
     }
 }
--- src/main/java/com/vastpro/service/QuestionService.java
+++ src/main/java/com/vastpro/service/QuestionService.java
@@ -1,6 +1,318 @@
 package com.vastpro.service;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ofbiz.entity.Delegator;
+import org.apache.ofbiz.entity.GenericEntityException;
+import org.apache.ofbiz.entity.GenericValue;
+import org.apache.ofbiz.entity.util.EntityQuery;
+import org.apache.ofbiz.service.DispatchContext;
+import org.apache.ofbiz.service.GenericServiceException;
+import org.apache.ofbiz.service.LocalDispatcher;
+import org.apache.ofbiz.service.ServiceUtil;
+
+public class QuestionService {
+	public static Map<String,Object>createQuestionService(DispatchContext dctx, Map<String, Object> questions){
+		LocalDispatcher dispatcher=dctx.getDispatcher();
+		try {
+			Delegator delegator = dctx.getDelegator();
+			
+			String topicId=(String)questions.get("topicId");
+			String questionDetail=(String)questions.get("questionDetail");
+			String questionTypeId=(String)questions.get("questionTypeId");
+			String answer = (String)questions.get("answer");
+			
+			
+			
+			if(topicId==null || questionDetail==null || answer==null  ) {
+				return ServiceUtil.returnError("topic Id and quetionDetail and answer are required");
+			}
+			
+			//check topic exists
+			GenericValue topic =EntityQuery.use(delegator).from("TopicMaster").where("topicId",topicId).queryOne();
+			
+			if(topic==null) {
+				return ServiceUtil.returnError("Topic not Found");
+			}
+			
+			 if (questionTypeId!= null) {
+		            GenericValue questionType = EntityQuery.use(delegator)
+		                .from("Enumeration")
+		                .where("enumId", questionTypeId, "enumTypeId", "QUESTION_TYPE")
+		                .queryOne();
+
+		            if (questionType == null) {
+		                return ServiceUtil.returnError("Invalid questionTypeId: " + questionTypeId);
+		            }
+		        }
+			
+		String questionId="SPX_QM_"+delegator.getNextSeqId("questionMaster");
+			questions.put("questionId",questionId);
+			dispatcher.runSync("createQuestion", questions);
+            
+			Map<String, Object> result = ServiceUtil.returnSuccess("Question created Successfully");
+            result.put("responseMessage", "Question created Successfully");
+            result.put("questionId", questionId);
+			return result;
+		}catch(GenericEntityException | GenericServiceException e) {
+			e.printStackTrace();
+			return ServiceUtil.returnError("Failed to create Question");
+		}
+	}
+	
+	//Update Questions
+	public static Map<String, Object> updateQuestion(DispatchContext dctx, Map<String, Object> context) {
+
+	    Delegator delegator = dctx.getDelegator();
+	    LocalDispatcher dispatcher=dctx.getDispatcher();
+
+	    try {
+	    	String questionId = (String) context.get("questionId"); 
+
+	        if (questionId.length()<3) {
+	            return ServiceUtil.returnError("For update QuestonId required");
+	        }
+
+	        GenericValue question = EntityQuery.use(delegator).from("questionMaster")
+	        				.where("questionId", questionId).queryOne();
+
+	        if (question == null) {
+	            return ServiceUtil.returnError("Question not present in database: " + questionId);
+	        }
+
+	        String questionDetail = (String)context.get("questionDetail");
+	        String optionA = (String)context.get("optionA");
+	        String optionB = (String)context.get("optionB");
+	        String optionC = (String)context.get("optionC");
+	        String optionD = (String)context.get("optionD");
+	        String answer = (String)context.get("answer");
+	        Long numAnswers = (Long)context.get("numAnswers");
+	        String questionTypeId = (String)context.get("questionTypeId");
+	        Long difficultyLevel = (Long)context.get("difficultyLevel");
+	        BigDecimal answerValue = (BigDecimal)context.get("answerValue");
+	        String topicId = (String)context.get("topicId");
+	        BigDecimal negativeMarkValue = (BigDecimal)context.get("negativeMarkValue");
+
+	        Map<String,Object> updateQuestion=new HashMap<>();
+           
+	        
+	        updateQuestion.put("questionId", questionId);
+	        updateQuestion.put("questionTypeId", questionTypeId);
+	        updateQuestion.put("questionDetail", questionDetail);
+	        updateQuestion.put("optionA", optionA);
+	        updateQuestion.put("optionB", optionB);
+	        updateQuestion.put("optionC", optionC);
+	        updateQuestion.put("optionD", optionD);
+	        updateQuestion.put("answer", answer);
+	        updateQuestion.put("numAnswers", numAnswers);
+	        updateQuestion.put("difficultyLevel", difficultyLevel);
+	        updateQuestion.put("answerValue", answerValue);
+	        updateQuestion.put("topicId", topicId);
+	        updateQuestion.put("negativeMarkValue", negativeMarkValue);
+	        
+
+	        if (topicId != null) {
+	            GenericValue topic = EntityQuery.use(delegator)
+	            		.from("TopicMaster")
+	            		.where("topicId", topicId)
+	            		.queryOne();
+
+	            if (topic == null) {
+	                return ServiceUtil.returnError("Topic not found for in database: " + topicId);
+	            }
+	            
+	        }
+
+	        if (questionTypeId!= null) {
+	            GenericValue questionType = EntityQuery.use(delegator)
+	                .from("Enumeration")
+	                .where("enumId", questionTypeId, "enumTypeId", "QUESTION_TYPE")
+	                .queryOne();
+
+	            if (questionType == null) {
+	                return ServiceUtil.returnError("Invalid questionTypeId: " + questionTypeId);
+	            }
+	            question.set("questionTypeId", questionTypeId);
+	        }
+
+	      
+	   Map<String,Object>result=  dispatcher.runSync("updateQuestion", updateQuestion);
+	        
+	        
+	   if(ServiceUtil.isError(result)) {
+			return ServiceUtil.returnError((String)result.get("errorMessage"));
+		}
+	   return ServiceUtil.returnSuccess("Question updated successfully");
+	        
+	        
+	    } catch (GenericEntityException | GenericServiceException e) {
+	        return ServiceUtil.returnError("Error updating question: " + e.getMessage());
+	    }
+	}
+	
+	
+	//DeleteQuestionservice
+	public static Map<String,Object> deleteQuestion(DispatchContext dctx, Map<String, Object> context){
+		LocalDispatcher dispatcher=dctx.getDispatcher();
+		Delegator delegator = dctx.getDelegator();
+		try {
+			 String questionId =  (String) context.get("questionId");
+			 
+			 GenericValue question = EntityQuery.use(delegator)
+					            .from("questionMaster")
+					            .where("questionId", questionId)
+					            .queryOne();
+			 
+			 if (question == null) {
+		            return ServiceUtil.returnError("Question not found for questionId: ");	      
+		        }
+			 
+			Map<String,Object>result= dispatcher.runSync("deleteQuestion", context);
+			
+			if(ServiceUtil.isError(result)) {
+				return ServiceUtil.returnError((String)result.get("errorMessage"));
+			}
+			
+			return ServiceUtil.returnSuccess("Question deleted successfully");
+		  
+			 
+		}catch(GenericEntityException | GenericServiceException e) {
+			return ServiceUtil.returnError("Error deleting question: " + e.getMessage());
+		}
+	}
+	
+	public static Map<String,Object> getQuestionsById(DispatchContext dctx,Map<String,Object> context){
+		Delegator delegator=dctx.getDelegator();
+		
+		try {
+			
+			String questionId=(String)context.get("questionId");
+			
+			
+			
+			GenericValue question=EntityQuery.use(delegator).from("questionMaster").where("questionId",questionId).queryOne();
+			
+			if(question == null) {
+				return ServiceUtil.returnError("question not Found");
+			}
+			
+			
+			Map<String,Object> oneQuestion=new HashMap<>();
+			
+			oneQuestion.put("question",question);
+			
+			Map<String,Object> result=ServiceUtil.returnSuccess();
+			result.put("question", oneQuestion);
+			
+			
+			
+			return result;
+		}catch(GenericEntityException e) {
+			return ServiceUtil.returnError("Error fetching questions By topic: "+e.getMessage());
+		}
+	}
+
+	
+	public static Map<String,Object> getQuestionsByTopic(DispatchContext dctx,Map<String,Object> context){
+		Delegator delegator=dctx.getDelegator();
+		
+		try {
+			
+			String topicId=(String)context.get("topicId");
+			Integer pageNo=(Integer)context.get("pageNo");
+			Integer pageSize=(Integer)context.get("pageSize");
+			
+			
+			
+			if(topicId==null || topicId.trim().isEmpty()) {
+				return ServiceUtil.returnError("topicId is Required");
+			}
+			
+			if(pageNo==null || pageNo<1) {
+				pageNo=1;
+			}
+			if(pageSize==null || pageSize<1) {
+				pageSize=10;
+			}
+			
+			GenericValue topic=EntityQuery.use(delegator).from("TopicMaster").where("topicId",topicId).queryOne();
+			
+			if(topic == null) {
+				return ServiceUtil.returnError("Topic not Found");
+			}
+			
+			
+			long totalCount=EntityQuery.use(delegator).from("questionMaster").where("topicId",topicId).queryCount();
+			
+			int totalPages=(int) Math.ceil((double)totalCount/pageSize);
+			int offset=(pageNo-1)*pageSize;
+			
+			
+			List<GenericValue> questions=EntityQuery.use(delegator)
+							.from("questionMaster")
+							.where("topicId",topicId)
+							.orderBy("questionId")
+							.cursorScrollInsensitive()
+							.maxRows(pageSize)
+							.queryList();
+			
+			if(offset>questions.size()) {
+				questions=new ArrayList<>();
+			}else {
+				questions=questions.subList(offset,
+								Math.min(offset+pageSize,questions.size()));
+			}
+			
+			List<Map<String,Object>> questionList=new ArrayList<>();
+			
+			for(GenericValue q:questions) {
+				Map<String,Object> qMap=new HashMap<>();
+				
+				qMap.put("questionId", q.getLong("questionId"));
+				qMap.put("questionDetail", q.getString("questionDetail"));
+				qMap.put("optionA", q.getString("optionA"));
+				qMap.put("optionB", q.getString("optionB"));
+				qMap.put("optionC", q.getString("optionC"));
+				qMap.put("optionD", q.getString("optionD"));
+				qMap.put("numAnswers", q.getLong("numAnswers"));
+				qMap.put("questionTypeId", q.getString("questionTypeId"));
+				qMap.put("difficultyLevel", q.getString("difficultyLevel"));
+				qMap.put("topicId", q.getString("topicId"));
+				qMap.put("negativeMarkValue", q.getBigDecimal("negativeMarkValue"));
+				
+				
+				questionList.add(qMap);
+			}
+			
+			Map<String,Object> result=ServiceUtil.returnSuccess();
+			
+			result.put("topicId", topic.getString("topicId"));
+			result.put("topicName", topic.getString("topicName"));
+			result.put("totalCount", totalCount);
+			result.put("questionList", questionList);
+			result.put("pageNo",pageNo);
+			result.put("pageSize",pageSize);
+			result.put("totalPages", totalPages);
+			result.put("hasNext", pageNo<totalPages);
+			result.put("hasPrevious",pageNo>1);
+			
+			
+			return result;
+		}catch(GenericEntityException e) {
+			return ServiceUtil.returnError("Error fetching questions By topic: "+e.getMessage());
+		}
+	}
+}
+
+
+//===========
+
+/*
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.Collections;
 import java.util.HashMap;
@@ -22,11 +334,11 @@ import org.apache.ofbiz.service.GenericServiceException;
 import org.apache.ofbiz.service.LocalDispatcher;
 import org.apache.ofbiz.service.ServiceUtil;
 
-/**
+
  * CreateQuestion
  * 
  * This class responsible for doing CRUD operation with questions
- */
+
 public class QuestionService {
 
 	public static Map<String, Object> createQuestionMaster(DispatchContext dctx, Map<String, Object> context) {
@@ -185,3 +497,4 @@ public class QuestionService {
     	     return ServiceUtil.returnError("Error: " + e.getMessage());
     	 }}
 }
+*/
