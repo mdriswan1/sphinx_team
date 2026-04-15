9a8e46e2f9f5f432104e5caf90a967408cb9d761
kamalraj51 <rkamalraj583@gmail.com>
2026-04-15 15:55:59 +0530

    end pont changed question

--- src/main/java/com/vastpro/service/ExamService.java
+++ src/main/java/com/vastpro/service/ExamService.java
@@ -146,7 +146,7 @@ public class ExamService {
 		Map<String, Object> result = ServiceUtil.returnSuccess("Topic getted successfully");
 		try {
 
-			List<GenericValue> topics = EntityQuery.use(delegator).from("ExamTopicDetail").queryList();
+			List<GenericValue> topics = EntityQuery.use(delegator).from("ExamTopicDetails").queryList();
 			if (topics.size() == 0) {
 				return ServiceUtil.returnSuccess("no topic found");
 
@@ -168,7 +168,7 @@ public class ExamService {
 		Map<String, Object> result = ServiceUtil.returnSuccess("Topic getted successfully");
 		try {
 
-			List<GenericValue> topics = EntityQuery.use(delegator).from("ExamTopicDetail").where("examId", examId).queryList();
+			List<GenericValue> topics = EntityQuery.use(delegator).from("ExamTopicDetails").where("examId", examId).queryList();
 			if (topics.size() == 0) {
 				return ServiceUtil.returnSuccess("no topic found");
 
