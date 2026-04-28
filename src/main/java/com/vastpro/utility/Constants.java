c88c7360a49869975f84634bcf6558b4043ab344
RENGANATHAN-2003 <renganmgl2003@gmail.com>
2026-04-28 10:16:22 +0530

    topic master entity updated -r

---
+++ src/main/java/com/vastpro/utility/Constants.java
@@ -0,0 +1,12 @@
+package com.vastpro.utility;
+
+public class Constants {
+
+	public static String QUESTION_MASTER = "QuestionMaster";
+
+	public static String TOPIC_MASTER = "TopicMaster";
+
+	public static String EXAM_MASTER = "ExamMaster";
+
+	public static final String QUESTION_BANK_MASTER_B = "TopicMaster";
+}
